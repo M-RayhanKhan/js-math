5862c46824f0bf5c4a3006da18f9b9e18ad2a36c
M-RayhanKhan <rayhansmartcode@gmail.com>
2024-09-01 10:47:28 +0600

    nan practice

--- math.js
+++ math.js
@@ -1,6 +1,26 @@
-var x = 5;
-var y = 6;
-var z = 2.3;
+// var x = 5;
+// var y = 6;
+// var z = 2.3;
 
-var sub = x / y;
-console.log( 5 + sub.toFixed(2));
+// var sub = x / y;
+// console.log( 5 + sub.toFixed(2));
+
+// 3^3
+// var x = 3;
+// console.log(x ** 3);
+
+// modulus %
+
+// console.log(10 / 2);
+
+// 10 / 3 bagfol bagses;
+// console.log(10 % 2);
+
+var x = 5;
+// x = x + 2;
+// x += 2;
+// x = x - 2;
+// x -= 2;
+// x++
+// x--
+// console.log(x);
---
+++ nan.js
@@ -0,0 +1,30 @@
+// isNaN()
+
+// not number, and can not be transformed into number = true 
+// number or can be transformed into number = false
+
+// var x = "rayhan";
+// var x = 5;
+// console.log(isNaN(x));
+
+// comparison operator (===, !==, >=, <=, >, < )
+
+var x = 5
+var y = 2
+var z = 8
+// console.log(x === y);
+// console.log(x === z);
+// console.log(x !== y);
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x >= y);
+// console.log(x <= y);
+
+// difference between === and ==
+// check only values
+// === check values with its data type
+
+var x = 5;
+var y = "5"
+console.log(x == y);
+console.log(x === y);
